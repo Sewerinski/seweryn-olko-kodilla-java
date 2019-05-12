efd317897e5ec6cbbfad93d914b55e3f01ba8d87
pc <seweryn.olko@gmail.com>
2019-05-12 23:39:27 +0200

    a

--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -1,12 +1,9 @@
 package com.kodilla.testing.user;
-
 public class SimpleUser {
-    private String username;
-
-    public SimpleUser(String username) {
+    String username;
+    public SimpleUser(String username){
         this.username = username;
     }
-
     public String getUsername() {
         return username;
     }
