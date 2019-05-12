763b541db64deff74cf51497330bbdeee070990a
pc <seweryn.olko@gmail.com>
2019-05-12 22:39:03 +0200

    aktualizacja

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public int add(int a, int b) {
+        int add = a + b;
+        return a+b;
+    }
+
+    public int substract(int a, int b) {
+        int substract = a - b;
+        return a-b;
+    }
+}
