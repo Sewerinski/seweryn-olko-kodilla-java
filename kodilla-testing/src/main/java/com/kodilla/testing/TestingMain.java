b3211be7058ae2e5fc31a627d43fac923f23a560
pc <seweryn.olko@gmail.com>
2019-05-12 22:33:27 +0200

    Aktualizacja

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(5,5);
+        int resultSubstract = calculator.substract(10,5);
+
+        if(resultAdd==10) {
+            System.out.println("Test ADD OK");
+        }else{
+            System.out.println("ERROR ADD");
+        }
+
+        if(resultSubstract==5) {
+            System.out.println("Test SUBSTRACT OK");
+        }else{
+            System.out.println("ERROR SUBSTRACT");
+        }
     }
 }
