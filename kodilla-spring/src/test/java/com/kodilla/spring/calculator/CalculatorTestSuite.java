1768b66d92858e653ca80493af4b56035a8dfabc
pc <seweryn.olko@gmail.com>
2019-08-02 17:19:12 +0200

    poprawienie zadania z kalkulatorem

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,11 +13,14 @@ public class CalculatorTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
         //When
-        calculator.add(1, 0);
-        calculator.sub(2, 1);
-        calculator.mul(1, 1);
-        calculator.div(2, 2);
+        double result = calculator.add(1, 0);
+        double result1 = calculator.sub(2, 1);
+        double result2 = calculator.mul(1, 1);
+        double result3 = calculator.div(2, 2);
         //Then
-        //do nothing
+        Assert.assertEquals(1, result, 1);
+        Assert.assertEquals(1, result1, 1);
+        Assert.assertEquals(1, result2, 1);
+        Assert.assertEquals(1, result3,1 );
     }
 }
