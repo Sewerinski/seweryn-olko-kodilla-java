534ae18b7ab4364ea06a8641f27bf76471b93dbd
pc <seweryn.olko@gmail.com>
2019-08-10 16:16:42 +0200

    zadanie 14.3 - mała poprawka

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -17,11 +17,24 @@ public class BoardTestSuite {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
         Board board = context.getBean(Board.class);
+
         board.getToDoList().getTasks().add("Odkurzanie podłogi w pokoju");
         board.getToDoList().getTasks().add("Zmywanie podłogi w pokoju");
         board.getInProgressList().getTasks().add("Mycie okien");
         board.getDoneList().getTasks().add("Łyk piwa przed ciężką pracą");
+
         //When&Then
+        System.out.println(board.getToDoList().getTasks());
+        System.out.println(board.getInProgressList().getTasks());
+        System.out.println(board.getDoneList().getTasks());
+
+        Assert.assertEquals("Odkurzanie podłogi w pokoju", board.getToDoList().getTasks().get(0));
         Assert.assertEquals("Zmywanie podłogi w pokoju", board.getToDoList().getTasks().get(1));
+        Assert.assertEquals("Mycie okien", board.getInProgressList().getTasks().get(0));
+        Assert.assertEquals("Łyk piwa przed ciężką pracą", board.getDoneList().getTasks().get(0));
+
+        Assert.assertEquals(2, board.getToDoList().getTasks().size());
+        Assert.assertEquals(1, board.getInProgressList().getTasks().size());
+        Assert.assertEquals(1, board.getDoneList().getTasks().size());
     }
 }
