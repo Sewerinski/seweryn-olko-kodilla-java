fdca21c269ad0cae14b5f1c1c1f63dcdcd4146d3
pc <seweryn.olko@gmail.com>
2019-08-10 20:20:11 +0200

    zadanie

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoardTestSuite {
@@ -18,23 +20,23 @@ public class BoardTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
         Board board = context.getBean(Board.class);
 
-        board.getToDoList().getTasks().add("Odkurzanie podłogi w pokoju");
-        board.getToDoList().getTasks().add("Zmywanie podłogi w pokoju");
-        board.getInProgressList().getTasks().add("Mycie okien");
-        board.getDoneList().getTasks().add("Łyk piwa przed ciężką pracą");
+        List<String> toDo = board.getToDoList().getTasks();
+        List<String> inProgress = board.getInProgressList().getTasks();
+        List<String> done = board.getDoneList().getTasks();
+
+        toDo.add("Odkurzanie podłogi w pokoju");
+        toDo.add("Zmywanie podłogi w pokoju");
+        inProgress.add("Mycie okien");
+        done.add("Łyk piwa przed ciężką pracą");
 
         //When&Then
-        System.out.println(board.getToDoList().getTasks());
-        System.out.println(board.getInProgressList().getTasks());
-        System.out.println(board.getDoneList().getTasks());
+        Assert.assertEquals("Odkurzanie podłogi w pokoju", toDo.get(0));
+        Assert.assertEquals("Zmywanie podłogi w pokoju", toDo.get(1));
+        Assert.assertEquals("Mycie okien", inProgress.get(0));
+        Assert.assertEquals("Łyk piwa przed ciężką pracą", done.get(0));
 
-        Assert.assertEquals("Odkurzanie podłogi w pokoju", board.getToDoList().getTasks().get(0));
-        Assert.assertEquals("Zmywanie podłogi w pokoju", board.getToDoList().getTasks().get(1));
-        Assert.assertEquals("Mycie okien", board.getInProgressList().getTasks().get(0));
-        Assert.assertEquals("Łyk piwa przed ciężką pracą", board.getDoneList().getTasks().get(0));
-
-        Assert.assertEquals(2, board.getToDoList().getTasks().size());
-        Assert.assertEquals(1, board.getInProgressList().getTasks().size());
-        Assert.assertEquals(1, board.getDoneList().getTasks().size());
+        Assert.assertEquals(2, toDo.size());
+        Assert.assertEquals(1, inProgress.size());
+        Assert.assertEquals(1, done.size());
     }
 }
