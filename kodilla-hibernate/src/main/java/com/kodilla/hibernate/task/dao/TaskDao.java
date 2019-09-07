57dae1beaf880a8a141bbbe933c51f84b3a0d478
pc <seweryn.olko@gmail.com>
2019-09-08 01:52:28 +0200

    another day with Hibernate - crud

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -0,0 +1,16 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface TaskDao extends CrudRepository<Task, Long> {
+    List<Task> findByDuration(int duration);
+    Optional<Task> findById (long id);
+}
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,47 @@
+package com.kodilla.hibernate.tasklist;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TASKLIST")
+public class TaskList {
+    private long id;
+    private String listName;
+    private String description;
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public long getId() {
+        return id;
+    }
+
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
